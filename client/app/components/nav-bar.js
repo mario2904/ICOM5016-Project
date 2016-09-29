b739c6210e0e1be15c932bd810e5895778454838
mario2904 <orbegoso2904@gmail.com>
2016-09-28 23:03:49 -0400

    Changed bootstrap theme. Added basic bootstrap Navbar.

--- client/app/components/nav-bar.js
+++ client/app/components/nav-bar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 export default class NavBar extends Component {
   render () {
     return (
       <div>
-        <h5> this is the nav-bar </h5>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">E-Spotter</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">Associations</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">MEEH</NavItem>
+            <NavItem eventKey={2} href="#">Login</NavItem>
+              <NavDropdown eventKey={3} title="Account" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Profile</MenuItem>
+                <MenuItem eventKey={3.2}>Settings</MenuItem>
+                <MenuItem eventKey={3.3}>Log Out</MenuItem>
+              </NavDropdown>
+          </Nav>
+        </Navbar>
+
         {this.props.children}
       </div>
     );
