600a11fc8f0e276e6401f4ae2b0ac02afe78fc24
mario2904 <orbegoso2904@gmail.com>
2016-10-08 20:53:28 -0400

    Added event list and search bar to the home view. This event list is supposed to list the user's interested events.

--- client/app/components/home.js
+++ client/app/components/home.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Button, PageHeader, Jumbotron } from 'react-bootstrap';
 
+import EventsSearchBar from './events-search-bar';
+import GridList from './grid-list';
+import EventsListItem from './events-list-item';
+
 export default class Home extends Component {
   render () {
     return (
@@ -11,7 +15,76 @@ export default class Home extends Component {
             <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <p><Button bsStyle="primary">Learn more</Button></p>
           </Jumbotron>
+          <h3 style={{textAlign: "center"}}>Interested Events</h3>
+          <EventsSearchBar />
+          <GridList items={this.props.interestedEvents} ListItem={EventsListItem}/>
       </div>
     );
   }
 }
+
+// For testing...
+Home.defaultProps = {
+  interestedEvents: [
+    {
+      name: "Hackathon",
+      association: "HackPR",
+      startDate: "Oct. 15, 2016",
+      endDate: "Oct. 16, 2016",
+      startHour: "9:00 am",
+      endHour: "5:00 pm",
+      location: "Roberto Clemente",
+      img: "http://hack.pr/wp-content/uploads/2016/09/Facebook-Banner-HackPR-1.png",
+      id: "1",
+      interested: 200
+    },
+    {
+      name: "Recogido de Alimentos",
+      association: "Organización Estudiantil de Protección de Animales",
+      startDate: "Oct. 13, 2016",
+      endDate: "Oct. 13, 2016",
+      startHour: "8:00 am",
+      endHour: "11:00 am",
+      location: "Universidad de Puerto Rico, Recinto Mayaguez",
+      img: "https://scontent-mia1-1.xx.fbcdn.net/v/t1.0-9/14590297_542744765930783_7339776675739874149_n.jpg?oh=8ad85c8fcb55d4fe6404c3aaf64ce685&oe=58A83C20",
+      id: "2",
+      interested: 20
+    },
+    {
+      name: "Fundraiser Neuro-RUM",
+      association: "Neuro-RUM",
+      startDate: "Sept. 28, 2016",
+      endDate: "Sept. 28, 2016",
+      startHour: "3:30 pm",
+      endHour: "8:30 pm",
+      location: "Cold Stone Creamery de Mayaguez",
+      img: "https://scontent-mia1-1.xx.fbcdn.net/t31.0-8/14242492_1122310254512514_1702488471920293701_o.jpg",
+      id: "3",
+      interested: 50
+    },
+    {
+      name: "Circuits Lab Workshop",
+      association: "IEEE Circuits and Systems Society UPRM",
+      startDate: "Oct. 5, 2016",
+      endDate: "Oct. 5, 2016",
+      startHour: "7:00 pm",
+      endHour: "9:00 pm",
+      location: "University of Puerto Rico, Mayaguez Campus - S-104A",
+      img: "https://scontent-mia1-1.xx.fbcdn.net/t31.0-8/14543654_1328305037188137_8919927744200733943_o.png",
+      id: "4",
+      interested: 35
+    },
+    {
+      name: "Smash Bros. Tournament",
+      association: "Idea Platform",
+      startDate: "Oct. 5, 2016",
+      endDate: "Oct. 5, 2016",
+      startHour: "6:00 pm",
+      endHour: "9:00 pm",
+      location: "University of Puerto Rico, Mayaguez Campus - S-228",
+      img: "https://scontent-mia1-1.xx.fbcdn.net/t31.0-8/14500634_881246661977335_7362168074202245899_o.jpg",
+      id: "5",
+      interested: 65
+    }
+  ]
+};
