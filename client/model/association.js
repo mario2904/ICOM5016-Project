309379c50e5b660e81eefd3493233ea38493ffa1
mario2904 <orbegoso2904@gmail.com>
2016-10-12 15:52:21 -0400

    Merged the server that rendered the react-app with the server that had all the backend logic REST API.

---
+++ client/model/association.js
@@ -0,0 +1,24 @@
+module.exports = class Association {
+
+  constructor (id, name, initials, location, link, email, password, profileImage=defaultImage, bio=defaultBio) {
+    this.id = id;
+    this.name = name;
+    this.initials = initials;
+    this.location = location;
+    this.link = link;
+    this.email = email;
+    this.password = password;
+    // Defaults
+    this.profileImage = profileImage;
+    this.bio = bio;
+  }
+}
+
+const defaultImage = "/images/defaults/default-profile.jpg";
+const defaultBio = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed \
+do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad \
+minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex\
+ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \
+velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat\
+cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id \
+est laborum.";
