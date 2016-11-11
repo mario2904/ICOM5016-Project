17d065fd9ac9661975d5c23e50ddd81d78f81f65
graciany <gracianylebron@gmail.com>
2016-11-10 22:34:33 -0400

    changed colors of the icons in the footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -9,16 +9,16 @@ export default class Footer extends Component {
       <Grid padded centered style={{height:"100px"}}>
         <Grid.Row style={{padding:"35px 0px 0px 0px"}}color="black" columns={4}>
           <Grid.Column>
-            <h5><Icon name="copyright"></Icon>E-Spotter</h5>
+            <h5><Icon color="orange"name="copyright"></Icon>E-Spotter</h5>
           </Grid.Column>
           <Grid.Column>
-            <Link style={{color:"white"}} to="#"><Icon name="help circle"></Icon>Help</Link>
+            <Link style={{color:"white"}} to="#"><Icon color="green"name="help circle"></Icon>Help</Link>
           </Grid.Column>
           <Grid.Column>
-            <Link style={{color:"white"}} to="#"><Icon name="info"></Icon>About Us</Link>
+            <Link style={{color:"white"}} to="#"><Icon color="violet" name="info circle"></Icon>About Us</Link>
           </Grid.Column>
           <Grid.Column>
-            <Link style={{color:"white"}} to="#"><Icon name="privacy"></Icon>Privacy/Terms</Link>
+            <Link style={{color:"white"}} to="#"><Icon color="yellow" name="privacy"></Icon>Privacy/Terms</Link>
           </Grid.Column>
         </Grid.Row>
       </Grid>
