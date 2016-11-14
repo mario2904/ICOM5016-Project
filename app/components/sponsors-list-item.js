53ae7df8a32b1fed4a2d49c0353cd0ff966d8a87
graciany <gracianylebron@gmail.com>
2016-11-14 17:40:15 -0400

    changed the names of the variables in sponsor list item and indv event component to be consistent with the backend

--- app/components/sponsors-list-item.js
+++ app/components/sponsors-list-item.js
@@ -3,7 +3,7 @@ import {Grid, Card, Image, Icon, Item, Segment} from 'semantic-ui-react';
 
 export default class SponsorsListItem extends Component {
   render () {
-    const { name, image, id} = this.props.item;
+    const { sponsor_name, image_path, sponsor_id, page_link} = this.props.item;
     return (
       <Grid.Column mobile={16} tablet={8} computer={4}>
       <Segment color="black">
