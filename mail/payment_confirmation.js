4ed57fba3af47e6f75793d4061f8a610e5781c95
graciany <gracianylebron@gmail.com>
2016-12-12 23:34:28 -0400

    added the mail temp for paymenst

--- mail/payment_confirmation.js
+++ mail/payment_confirmation.js
@@ -1,34 +1,44 @@
-var sg = require('./index');
-var helper = require('sendgrid').mail;
+ar helper = require('sendgrid').mail;
+var from_email = new helper.Email('espotter.0@gmail.com');
+var to_email = new helper.Email('gracianylebron@gmail.com');
+const first_name ="Graciany";
+var subject = `Payment Comfirmation 11!!!!! ${first_name}!`; //This replales the subject tag
+const university ="University of Puerto Rico Mayagüez";
+ var content = new helper.Content('text/html', ' ');
+ 
+const last_name ="Lebron";
+const event_name ="All Nighter Study Session";
+const association_name="SHPE";
+const association_email="shpe@upr.edu";
+const amount ="$5.99";
+const transaction_id="1";
+var mail = new helper.Mail(from_email, subject, to_email, content);
+mail.personalizations[0].addSubstitution(
+  new helper.Substitution('-amount-', amount));
+mail.personalizations[0].addSubstitution(
+  new helper.Substitution('-first_name-', first_name));
+mail.personalizations[0].addSubstitution(
+  new helper.Substitution('-last_name-', last_name));
+mail.personalizations[0].addSubstitution(
+  new helper.Substitution('-event_name-', event_name));
+mail.personalizations[0].addSubstitution(
+  new helper.Substitution('-association_name-', association_name));
+mail.personalizations[0].addSubstitution(
+  new helper.Substitution('-association_email-', association_email));
+mail.personalizations[0].addSubstitution(
+  new helper.Substitution('-transaction_id-', transaction_id));
 
-module.exports = function(first_name, last_name, college, email) {
+mail.setTemplateId('6f9ca928-d991-4527-82c0-f88d5ba7875e');
 
-  var from_email = new helper.Email('espotter.0@gmail.com');
-  var to_email = new helper.Email(email);
-  var subject = `Payment Transfered`; //This replales the subject tag
-  var content = new helper.Content(
-    'text/html', `<p style=color:"black">Hi ${first_name} ${last_name}, congrats on your new account.</p>
-    <p style=color:"black">Awesome, now you can track the events of your favorite associations at ${college}.
-    Hope you enjoy our app.<p>`);
-    //this replaces the body tag
-  var mail = new helper.Mail(from_email, subject, to_email, content);
-  mail.personalizations[0].addSubstitution(
-    new helper.Substitution('-name-', 'Example User'));
-  mail.personalizations[0].addSubstitution(
-    new helper.Substitution('-city-', 'Denver'));
-  mail.setTemplateId('b2c80990-8ddc-4dec-a065-ee114f344f6b');
+var sg = require('sendgrid')('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');
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
+});
 
-
-  var request = sg.emptyRequest({
-    method: 'POST',
-    path: '/v3/mail/send',
-    body: mail.toJSON(),
-  });
-
-  sg.API(request, function(error, response) {
-    console.log(response.statusCode);
-    console.log(response.body);
-    console.log(response.headers);
-  });
-
-}
+sg.API(request, function(error, response) {
+  console.log(response.statusCode);
+  console.log(response.body);
+  console.log(response.headers);
+});
